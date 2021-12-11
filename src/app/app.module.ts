621be37d2c97b05435215d829e5c37b87f59801f
m.karimi <m.karimi@hoshmandrisk.ir>
2021-12-11 10:03:01 +0330

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      TestComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
